71c043ef8f8fc01e388732e0f45d6137fce32f43
Gerard McE <Ger@email.com>
2023-06-10 21:22:51 +0000

    FirstCommit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace schoolManagementApp.mvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
